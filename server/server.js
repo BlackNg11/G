7d8729feae8365e6ac470c439fc493f1d24ca95f
Black <blackng10.5.1997@gmail.com>
2021-01-26 19:20:54 +0700

    'update

--- server/server.js
+++ server/server.js
@@ -375,11 +375,6 @@ app.get("/api/users/removeFromCart", auth, (req, res) => {
 	);
 });
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 const port = process.env.PORT || 3002;
 
